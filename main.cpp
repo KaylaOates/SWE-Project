cd7be5f0eb202fec602f8e19ca4021b13c56eeb5
DeveshP <73601434+Devesh-PatelUF@users.noreply.github.com>
2022-03-28 15:51:31 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,23 +5,33 @@
 
 int main(int argc, char* const argv[])
 {
-	int result = Catch::Session().run(argc, argv);
+	
+    //running test before anything happens
+    //Make sure that you create the linkLogger instance before this line of code as it completely writes over the file, so all the data from before will be lost.
+    int result = Catch::Session().run(argc, argv);
+    
+    
     {
-        sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-        sf::CircleShape shape(100.f);
-        shape.setFillColor(sf::Color::Green);
+        //declare all window, shape, text, font objects here
+        sf::RenderWindow window(sf::VideoMode(1480, 1024), "Link Logger");
 
         while (window.isOpen())
         {
             sf::Event event;
+            //this while loop is where you will check for events, like checking for a box to be left clicked, right clicked, check for typing to happen ect.
+            //If anyone is making a textbox that the user will write into ask me for help because I have a code for how to do that really easily - Devesh
+
             while (window.pollEvent(event))
             {
                 if (event.type == sf::Event::Closed)
                     window.close();
             }
 
-            window.clear();
-            window.draw(shape);
+            //SFML should alwasy have a clear, draw, display statement set here. Clear will clear the screen of previous elements, draw will draw what needs to be drawn to the screen
+            //display will display it to the screen. If you need to change what is being shown on the screen, change what is being drawn to the screen
+            window.clear(sf::Color::White);
+            //DRAW STATEMENTS GOES HERE
+            // window.draw(/*thing to be draw to the screen goes here */);
             window.display();
         }
 
